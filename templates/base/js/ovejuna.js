b53359eb3434ecf3cf73b970fe8272ec924758d2
Juan Benavides Romero <jbalde@gmail.com>
2013-04-14 12:17:06 +0200

    add project have a modal functionality

--- templates/base/js/ovejuna.js
+++ templates/base/js/ovejuna.js
@@ -1,3 +1,9 @@
 !function ($) {
-	
+	$(document).on("ready", function() {
+		var projectAddLayer = $("#project-add-layer");
+		projectAddLayer.find("a[data-action=submit]").on("click", function() {
+			projectAddLayer.find("form").submit();
+			return false;
+		});
+	});
 }(window.jQuery);
